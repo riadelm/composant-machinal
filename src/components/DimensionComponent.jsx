216d03c2f3a6cc5277d751738ec9cb5f5f6e6208
riadelm <elmahmoudy.riad@gmail.com>
2024-02-26 22:51:32 -0500

    restructure

---
+++ src/components/DimensionComponent.jsx
@@ -0,0 +1,75 @@
+import React, { useRef, useMemo, useEffect, useCallback} from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
+// import { useTexture } from '@react-three/drei';
+import * as THREE from 'three';
+import vertexShader from '../shaders/vertexShader.js';
+import fragmentShader from '../shaders/fragmentShader.js';
+
+const targetFPS = 30; 
+const frameInterval = 1 / targetFPS;
+
+const DimensionComponent = () => {
+  const mouseRef = useRef(new THREE.Vector2());
+  const shaderMaterialRef = useRef();
+  const lastUpdateTimeRef = useRef(0);
+
+  const handleMouseMove = useCallback((event) => {
+    mouseRef.current.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouseRef.current.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  }, [mouseRef]);
+
+  useEffect(() => {
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, [handleMouseMove]);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const { innerWidth, innerHeight } = window;
+      const shaderMaterial = shaderMaterialRef.current;
+      if (shaderMaterial) {
+        shaderMaterial.uniforms.resolution.value.set(innerWidth, innerHeight);
+      }
+    };
+    
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initialize with current size
+  
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+  
+
+  
+  const uniforms = useMemo(() => ({
+    time: { value: 0 },
+    resolution: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+    mouse: { value: mouseRef.current},
+  }), []);
+
+  useFrame(({ clock, size }) => {
+    const elapsedTime = clock.getElapsedTime();
+    if (elapsedTime - lastUpdateTimeRef.current > frameInterval) {
+      const shaderMaterial = shaderMaterialRef.current;
+      if (shaderMaterial) {
+        shaderMaterial.uniforms.time.value = elapsedTime;
+        shaderMaterial.uniforms.resolution.value.set(size.width, size.height);
+        shaderMaterial.uniforms.mouse.value = mouseRef.current;
+      }
+      lastUpdateTimeRef.current = elapsedTime;
+    }
+  });
+
+  return (
+    <mesh>
+      <planeGeometry args={[2, 2]} />
+      <shaderMaterial
+        ref={shaderMaterialRef}
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+        uniforms={uniforms}
+      />
+    </mesh>
+  );
+};
+
+export default DimensionComponent;
---
+++ src/views/ApplicationForm.js
@@ -0,0 +1,128 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import emailjs from 'emailjs-com';
+import './ApplicationForm.css';
+import DimensionComponent from '../components/DimensionComponent';
+import { Canvas } from '@react-three/fiber';
+import * as LR from '@uploadcare/blocks';
+import { Widget } from '@uploadcare/react-widget';
+
+LR.registerBlocks(LR);
+
+const ApplicationForm = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const isAffiliated = watch("affiliation", "no"); // Watching the radio button
+    const { innerWidth: width, innerHeight: height } = window;
+    const [fileUrl, setFileUrl] = useState('')
+
+    const onSubmit = data => {
+        console.log(data);
+        const formData = {
+            fullName: data.fullName,
+            email: data.email,
+            pronouns: data.pronouns,
+            dob: data.dob,
+            location: data.location,
+            artistBio: data.artistBio,
+            fileUrl: fileUrl,
+            //upload file request from dropbox or ask for follow up email with 
+            affiliation: data.affiliation,
+            affiliationDetails: data.affiliationDetails,
+            additionalInfo: data.additionalInfo
+        };
+        console.log(formData)
+
+        emailjs.send('service_xv9wkqe', 'template_zujncxw', formData, 'slEBWGhWka7S9f55f')
+            .then((result) => {
+                console.log(result.text);
+                alert("Application submitted successfully!");
+            }, (error) => {
+                console.log(error.text);
+                alert("Failed to submit the application. Please try again.");
+            });
+    };
+    return (
+        <div className="Apply">
+            <Canvas className="MainSpace" camera={{ position: [0, 0, 1], aspect: width / height, fov: 100 }} gl={{ antialias: false, stencil: false, depth: false }} >
+                <ambientLight intensity={0.5} />
+                <DimensionComponent />
+            </Canvas>
+            <div className="form-container">
+                <form className="application-form" onSubmit={handleSubmit(onSubmit)}>
+                    <h2>Open Call for Artists - Application Form</h2>
+                    <h2>Deadline: March 31st 2024</h2>
+                    <div className="form-group">
+                        <label htmlFor="fullName">Full Name</label>
+                        <input type="text" id="fullName" {...register("fullName", { required: true })} />
+                        {errors.fullName && <span>This field is required</span>}
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email Address</label>
+                        <input type="email" id="email" {...register("email", { required: true })} />
+                        {errors.email && <span>This field is required</span>}
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="pronouns">Pronouns</label>
+                        <input type="text" id="pronouns" {...register("pronouns", { required: true })} />
+                        {errors.pronouns && <span>This field is required</span>}
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="dob">Date of Birth</label>
+                        <input type="date" id="dob" {...register("dob", { required: true })} />
+                        {errors.dob && <span>This field is required</span>}
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="location">Location</label>
+                        <input type="text" id="location" {...register("location", { required: true })} />
+                        {errors.pronouns && <span>This field is required</span>}
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="artistBio">Artist Bio</label>
+                        <textarea id="artistBio" {...register("artistBio", { required: true })} />
+                        {errors.artistBio && <span>This field is required</span>}
+                    </div>
+                    <div className="form-group">
+                        
+                        <label htmlFor="file">Portfolio File (optional)*</label>
+                        <p className='asterisk'> Please include your name in the portfolio file name</p>
+                        <p className='asterisk'>* For large files (videos, files larger than 10 MB), please email composantm@gmail.com with your Portfolio file(s) as an attachement and your name in the subject</p>
+                        <Widget
+                            publicKey="cfdb03f9e709e945d733"
+                            id="file"
+                            onChange={(fileInfo) => setFileUrl(fileInfo.cdnUrl)}
+                        />
+                        {fileUrl && <p>File uploaded successfully: <a href={fileUrl}>View File</a></p>}
+                    </div>
+
+                    <div className="form-group">
+                        <label>Are you affiliated with a school or organization?</label>
+                        <label><input type="radio" value="yes" {...register("affiliation", { required: true })} /> Yes</label>
+                        <label><input type="radio" value="no" {...register("affiliation", { required: true })} /> No</label>
+                        {errors.affiliation && <span>This field is required</span>}
+                    </div>
+
+                    {isAffiliated === "yes" && (
+                        <div className="form-group">
+                            <label htmlFor="affiliationDetails">Affiliation Details</label>
+                            <input type="text" id="affiliationDetails" {...register("affiliationDetails", { required: true })} />
+                            {errors.affiliationDetails && <span>This field is required</span>}
+                        </div>
+                    )}
+
+                    <div className="form-group">
+                        <label htmlFor="additionalInfo">Any additional information/questions? (Ex: social media handle, links to portfolio, video, proposal, etc.)</label>
+                        <textarea id="additionalInfo" {...register("additionalInfo")} />
+                    </div>
+                    
+                    <button type="submit">Submit Application</button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default ApplicationForm;
